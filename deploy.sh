d263fd2efaad10642a33f43e64e574baeea255ac
Stef Ruinard <41334908+Sruinard@users.noreply.github.com>
2023-03-25 14:17:51 +0000

    remove venv and update deployment.

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,14 @@
 # 3. Create an App Service with a python runtime and use github as a deployment source and use the dev branch and build the app
 # 4. Build application during deployment using SCM_DO_BUILD_DURING_DEPLOYMENT=true
 # 5. enable cors for the app service
-# 6. Set the configuration startup file to use gunicorn
+# 6. Set the configuration startup file to use gunicorn and use uvicorn workers
 
 
 # AUTOCOMPLETES FROM HERE (SET VARIABLES BY YOURSELF):
 resourceGroupName="csu-nl-sr-asr-rg"
 location="westeurope"
-appServicePlanName="csu-nl-sr-asr-appserviceplan1"
-webAppName="csu-nl-sr-asr-webapp1"
+appServicePlanName="csu-nl-sr-asr-appserviceplan2"
+webAppName="csu-nl-sr-asr-webapp2"
 repoUrl="https://github.com/Sruinard/building-an-app-through-nlp"
 branch="dev"
 
@@ -30,8 +30,7 @@ az webapp config appsettings set --name $webAppName --resource-group $resourceGr
 # 5. enable cors for the app service
 az webapp cors add --allowed-origins "*" --name $webAppName --resource-group $resourceGroupName
 
-# 6. Set the configuration startup file to use gunicorn
-az webapp config set --startup-file "gunicorn --bind='0.0.0.0' --timeout 600 app:app" --name $webAppName --resource-group $resourceGroupName
-
+# 6. Set the configuration startup file to use gunicorn and use uvicorn workers
+az webapp config set --startup-file "gunicorn --bind='0.0.0.0' --workers=4 --timeout=120 --worker-class=uvicorn.workers.UvicornWorker app:app" --name $webAppName --resource-group $resourceGroupName
 
 
